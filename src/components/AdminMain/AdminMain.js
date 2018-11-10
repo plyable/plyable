be2818b7c06ade948b2c10dd835130566fb1ab83
Daniel Ridley <ridleydan31@gmail.com>
2018-11-10 10:40:31 -0600

    make h1

--- src/components/AdminMain/AdminMain.js
+++ src/components/AdminMain/AdminMain.js
@@ -1,2 +1,26 @@
-// Scope: Organization List
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class AdminMain extends Component {
+    state = {
+        email: '',
+        password: '',
+    };
+
+
+    render() {
+        return (
+            <div>
+                <h1>Organization List</h1>
+
+            </div>
+        );
+    }
+}
+
+const mapStateToProps = state => ({
+    errors: state.errors,
+});
+
+export default connect(mapStateToProps)(AdminMain);
+
