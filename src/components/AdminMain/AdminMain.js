c9e32b3b4b8dc787a0c58c9ef0e20a6d6c3d593c
Daniel Ridley <ridleydan31@gmail.com>
2018-11-10 11:31:23 -0600

    add GET route for organization names

--- src/components/AdminMain/AdminMain.js
+++ src/components/AdminMain/AdminMain.js
@@ -7,11 +7,16 @@ class AdminMain extends Component {
         password: '',
     };
 
-
+    handleClick = () => {
+        console.log('button working');
+    }
     render() {
         return (
             <div>
                 <h1>Organization List</h1>
+
+
+                <button onClick={this.handleClick}>Add New Organization</button>
 
             </div>
         );
