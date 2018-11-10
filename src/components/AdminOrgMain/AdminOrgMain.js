ec07ca01c3911131609c8faff85267e01ba6c5d0
Manny Jo <jomansang@gmail.com>
2018-11-10 10:02:58 -0600

    Make login work

--- src/components/AdminOrgMain/AdminOrgMain.js
+++ src/components/AdminOrgMain/AdminOrgMain.js
@@ -1,1 +1,14 @@
 // Scope: Organization Specific Data (charts)
+import React, { Component } from 'react';
+
+class AdminOrgMain extends Component {
+    render() {
+        return (
+            <div>
+                AdminOrgMain
+            </div>
+        );
+    }
+}
+
+export default AdminOrgMain;
