892921d287cb8c467b31b2bc854043f5c0b3903d
Elias Friedman <eliasfriedman@Eliass-MacBook-Pro.local>
2018-11-14 09:49:19 -0600

    default security_level for users set to 2

--- database.sql
+++ database.sql
@@ -21,7 +21,7 @@ CREATE TABLE "user" (
 	"org_id" INT REFERENCES "organization",
 	"password" VARCHAR(1000),
 	"email" VARCHAR(30) UNIQUE NOT NULL,
-	"security_level" INT DEFAULT 0,
+	"security_level" INT DEFAULT 2,
 	"temp_key" VARCHAR(1000),
 	"temp_key_timeout" TIMESTAMP
 );
