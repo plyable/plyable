8601e87b91cfb9e70d433ac9d56130f0af1c10e1
Elias Friedman <eliasfriedman@Eliass-MacBook-Pro.local>
2018-11-16 14:31:10 -0600

    functional component and reducer made to manage dialogs and toasts

---
+++ src/redux/reducers/messagesReducer.js
@@ -0,0 +1,18 @@
+const messagesReducer = (state = {
+    loginError: false,
+    deactivedOrganization: false,
+    addedEmployees: false,
+    addedManagers: false,
+    surveySubmitted: false,
+}, action) => {
+    //each component of state corresponds to a dialog that lives on app.js
+    //on actions that trigger this reducer open or close one of these dialogs
+    switch (action.type) {
+        default:
+            return state;
+    }
+};
+
+// user will be on the redux state at:
+// state.user
+export default messagesReducer;
