cfa8f9545a50534c684a4e98c322dd1f98569f87
Manny Jo <jomansang@gmail.com>
2018-11-20 13:52:00 -0600

    recover old new code

--- src/redux/reducers/surveyReducer.js
+++ src/redux/reducers/surveyReducer.js
@@ -17,24 +17,6 @@ const surveyScore = (state = [], action) => {
   }
 };
 
-const expectationScore = (state = [], action) => {
-  switch (action.type) {
-    case 'SET_EXPECTATION_SCORE':
-      return [ ...state, action.payload ];
-    case 'REMOVE_EXPECTATION_BEHAVIOR':
-      return state.filter(survey => {
-        if(survey.id === action.payload.id) {
-          return false;
-        } else {
-          return true;
-        }
-      });
-    default:
-      return state;
-  }
-};
-
 export default combineReducers({
   surveyScore,
-  expectationScore,
 });//stores survey scores from server
