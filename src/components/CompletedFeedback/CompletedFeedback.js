bba2fd99e92527903758802eb6579533481d3feb
Elias Friedman <eliasfriedman@Eliass-MacBook-Pro.local>
2018-11-20 15:05:02 -0600

    list paginated

--- src/components/CompletedFeedback/CompletedFeedback.js
+++ src/components/CompletedFeedback/CompletedFeedback.js
@@ -1,7 +1,13 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { TablePagination } from '@material-ui/core';
 
 class CompletedFeedback extends Component {
+
+    state = {
+        rowsPerPage: 6,
+        page: 0,
+    }
 
     componentDidMount() {
         const arr = window.location.hash.split('/');
@@ -14,8 +20,22 @@ class CompletedFeedback extends Component {
         this.props.dispatch({ type: 'FETCH_PARTICIPATION', payload: id });
     }
 
+    handleChangeRowsPerPage = event => {
+        this.setState({
+            ...this.state,
+            rowsPerPage: event.target.value
+        });
+    };
+    handleChangePage = (event, page) => {
+        this.setState({
+            ...this.state,
+            page,
+        });
+    };
+
 
     render() {
+        const { rowsPerPage, page } = this.state;
         let employees = this.props.reduxState.participationReducer;
         let uncompleted = employees.filter(employee => Number(employee.count) === 0);
         return (
@@ -23,8 +43,24 @@ class CompletedFeedback extends Component {
                 <h4>{employees.length - uncompleted.length}/{employees.length} {this.props.reduxState.adminMainReducer.name} employees have completed their survey</h4>
                 <h3>Awaiting Response From...</h3>
                 <ul>
-                    {uncompleted.map(employee => <li key={employee.email}>{employee.email}</li>)}
+                    {uncompleted.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                        .map(employee => <li key={employee.email}>{employee.email}</li>)}
                 </ul>
+                <TablePagination
+                    rowsPerPageOptions={[6, 12, 24]}
+                    component="div"
+                    count={uncompleted.length}
+                    rowsPerPage={rowsPerPage}
+                    page={page}
+                    backIconButtonProps={{
+                        'aria-label': 'Previous Page',
+                    }}
+                    nextIconButtonProps={{
+                        'aria-label': 'Next Page',
+                    }}
+                    onChangePage={this.handleChangePage}
+                    onChangeRowsPerPage={this.handleChangeRowsPerPage}
+                />
             </div >
         );
     }
